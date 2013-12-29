58d3237ac2d2ed44dbc72a55cd39f40f899cdf4a
Emily Janzer <emily.janzer@gmail.com>
2013-12-29 00:19:07 -0800

    Added sockets.io

--- app.js
+++ app.js
@@ -27,9 +27,21 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+	socket.emit('news', {hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
 });
